3a6a31bd7eca61272b19fae88974c985a7916a84
Rita Hammett <hammett.rc@gmail.com>
2020-06-11 20:41:16 -0400

    MOD_04-Demo-Lesson_01-Steps_13-14

--- Democode/01_ControllersExample_begin/ControllersExample/HomeController.cs
+++ Democode/01_ControllersExample_begin/ControllersExample/HomeController.cs
@@ -22,5 +22,13 @@ namespace ControllersExample
         {
             return Content("My param is: " + id);
         }
+
+        public IActionResult RouteDataExample()
+        {
+            string controller = (string)RouteData.Values["Controller"];
+            string action = (string)RouteData.Values["action"];
+            string id = (string)RouteData.Values["id"];
+            return Content($"Action information: the action is in {controller} controller, the action name is {action} and the id value is {id}.");
+        }
     }
 }
