4a38557d59c1fdd805f03653f5ba7940c326c14e
Rita Hammett <hammett.rc@gmail.com>
2020-06-11 22:00:26 -0400

    MOD_04-Demo-Lesson_02-Steps_11-19

---
+++ Democode/02_RoutesExample_begin/RoutesExample/Controllers/CalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoutesExample.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult MultByTwo(int num)
+        {
+            int result = num * 2;
+            return Content(result.ToString());
+        }
+
+        [HttpGet("Divide/{param?}")]
+        public IActionResult DivideByTen(int param)
+        {
+            int result = param / 10;
+            return Content(result.ToString());
+        }
+    }
+}
