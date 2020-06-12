d0553083d9a5c0970918f624bd39bf9a5764aac7
Rita Hammett <hammett.rc@gmail.com>
2020-06-11 20:12:38 -0400

    MOD_04-Demo-Lesson_01-Steps_1-10

---
+++ Democode/01_ControllersExample_begin/ControllersExample/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ControllersExample.Models;
+
+namespace ControllersExample
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ExampleModel model = new ExampleModel()
+            { 
+                Sentence = "Welcome to Module 4 - Demo 1" 
+            };
+            return View(model);
+        }
+    }
+}
